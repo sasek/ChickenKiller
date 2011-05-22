81537c6379b67dde37205c4f24ed728426bcc256
sasek <say.gaberc@gmail.com>
2011-05-22 16:42:31 +0200

    dela?

---
+++ src/edu/sasq/Chickenkiller/android/Game.java
@@ -0,0 +1,88 @@
+package edu.sasq.Chickenkiller.android;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class Game extends Activity {
+    static TextView count;
+    public int bullet;
+	Odstevanje stej= new Odstevanje(120000,1000);
+	
+	
+	
+	@Override
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        bullet=5;
+	        setContentView(R.layout.game);
+	        stej.start();
+	        count=(TextView) findViewById(R.id.txtCas);
+	        
+	        // Touch koordinate
+	        
+	        final TextView textView = (TextView)findViewById(R.id.textView1); 
+	        // this is the view on which you will listen for touch events 
+	        final View touchView = findViewById(R.id.view1); 
+	        touchView.setOnTouchListener(new View.OnTouchListener() { 
+	        @Override 
+	        public boolean onTouch(View v, MotionEvent event) {
+	        	if(bullet>0)
+	        	bullet--;
+	        	else
+	        		
+	        	{// Se ne pokaže...
+	        		//Toast.makeText(Game.this, "Reload!!",Toast.LENGTH_SHORT);
+	        		bullet=5;
+	        	}
+	        	textView.setText(""+bullet);
+	        /*textView.setText("Touch coordinates : " + 
+	        String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));*/ 
+	        return true; 
+	        } 
+	        }); 
+	        
+	 }
+	
+
+
+public class Odstevanje extends CountDownTimer{
+
+	public Odstevanje(long millisInFuture, long countDownInterval) {
+	super(millisInFuture, countDownInterval);
+	}
+
+	@Override
+	public void onFinish() {
+		finish();
+	}
+
+	@Override
+	public void onTick(long millisUntilFinished) {
+		long s=0;
+		int min=0;
+		if(millisUntilFinished/60000==1)
+		{
+		min=1;
+			s=(millisUntilFinished/1000)-60;
+		}
+		else
+		{
+			s=millisUntilFinished/1000;
+			min=0;
+		}
+		
+			
+	Game.count.setText(min+":"+s);
+
+	}
+
+	}
+
+}
+
---
+++ src/edu/sasq/Chickenkiller/android/mainmenu.java
@@ -0,0 +1,57 @@
+package edu.sasq.Chickenkiller.android;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class mainmenu extends Activity {
+    /** Called when the activity is first created. */
+	public static final int GAME_ACTIVITY_ID = 1;
+    @Override
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    public void clickStart(View v)
+    {
+    	Intent igra=new Intent(this,Game.class);
+    	this.startActivity(igra);
+    }
+    public void clickQuit(View v)
+    {
+    	this.finish();
+    	
+    }
+    public void clickScore(View v)
+    {
+    	
+    }
+    public void onResume()
+    {
+    	super.onResume();
+    }
+    public void onStart()
+    {
+    	super.onStart();
+    }
+    public void onPause()
+    {
+    	super.onPause();
+    }
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        // See which child activity is calling us back.
+
+	if(resultCode==-1)
+	{
+		finish();
+		
+	}
+	
+}
+    
+}
