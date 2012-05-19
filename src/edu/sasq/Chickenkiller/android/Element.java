265fe8b1447a752bbff278f1cf2b40279a794929
Saso <Saso@Saso-PC.home>
2012-05-19 19:26:11 +0200

    pripravljene animacije

--- src/edu/sasq/Chickenkiller/android/Element.java
+++ src/edu/sasq/Chickenkiller/android/Element.java
@@ -1,11 +1,14 @@
 package edu.sasq.Chickenkiller.android;
 
+import java.util.ArrayList;
+
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
-public class Element {
+public class Element
+{
 	public float mX;
 	public float mY;
 	private int points;
@@ -14,10 +17,24 @@ public class Element {
 	public double mSpeedX;
 	public double mSpeedY;
 	public boolean killer;
-	private Bitmap mBitmap;
 
-	public Element(Resources res, int x, int y, int p) {
+	private int positionBitmap = 0;
+	private long timeToChangeBitmap;
+	private boolean isAnimated;
+
+	public ArrayList<Bitmap> AnimatedBitmaps = new ArrayList<Bitmap>();
+
+	public Bitmap mBitmap;
+
+	public Element(Resources res, int x, int y, int p)
+	{
+	
 		mBitmap = BitmapFactory.decodeResource(res, R.drawable.finish);
+		AnimatedBitmaps.add(mBitmap);
+		AnimatedBitmaps.add(BitmapFactory
+				.decodeResource(res, R.drawable.dontkill));
+		AnimatedBitmaps.add(BitmapFactory
+				.decodeResource(res, R.drawable.gabi48));
 		mX = x + (mBitmap.getWidth() / 2);
 		mY = y + (mBitmap.getHeight() / 2);
 		slikaX = mBitmap.getWidth() / 2;
@@ -26,10 +43,19 @@ public class Element {
 		mSpeedX = 1;
 		mSpeedY = 0;
 		killer = false;
+		isAnimated = true;
+		timeToChangeBitmap = 0;
+
 	}
 
-	public Element(Resources res, int x, int y, int p, double sX, double sY) {
+	public Element(Resources res, int x, int y, int p, double sX, double sY)
+	{
 		mBitmap = BitmapFactory.decodeResource(res, R.drawable.finish);
+		AnimatedBitmaps.add(mBitmap);
+		AnimatedBitmaps.add(BitmapFactory
+				.decodeResource(res, R.drawable.dontkill));
+		AnimatedBitmaps.add(BitmapFactory
+				.decodeResource(res, R.drawable.gabi48));
 		mX = x + (mBitmap.getWidth() / 2);
 		mY = y + (mBitmap.getHeight() / 2);
 		points = p;
@@ -38,21 +64,29 @@ public class Element {
 		slikaX = mBitmap.getWidth() / 2;
 		slikaY = mBitmap.getHeight() / 2;
 		killer = false;
+		isAnimated = true;
+		timeToChangeBitmap = 0;
 	}
 
-	public Element(Resources res, int x, int y) {
+	public Element(Resources res, int x, int y)
+	{
 		mBitmap = BitmapFactory.decodeResource(res, R.drawable.tank);
 		mX = x + (mBitmap.getWidth() / 2);
 		mY = y + (mBitmap.getHeight() / 2);
 		slikaX = mBitmap.getWidth();
 		slikaY = mBitmap.getHeight();
+		isAnimated = false;
 
 	}
 
-	public Element(Resources res, int x, int y, boolean z) {
-		if (z) {
+	public Element(Resources res, int x, int y, boolean z)
+	{
+		if (z)
+		{
 			mBitmap = BitmapFactory.decodeResource(res, R.drawable.healthcrate);
-		} else {
+		}
+		else
+		{
 			mBitmap = BitmapFactory.decodeResource(res, R.drawable.ammo);
 		}
 		mX = x + (mBitmap.getWidth() / 2);
@@ -60,10 +94,12 @@ public class Element {
 		slikaX = mBitmap.getWidth();
 		slikaY = mBitmap.getHeight();
 		mSpeedY = 2.5;
+		isAnimated = false;
 
 	}
 
-	public Element(Resources res, int x, int y, double sX, double sY) {
+	public Element(Resources res, int x, int y, double sX, double sY)
+	{
 		mBitmap = BitmapFactory.decodeResource(res, R.drawable.metek);
 		mX = x + (mBitmap.getWidth() / 2);
 		mY = y + (mBitmap.getHeight() / 2);
@@ -71,35 +107,60 @@ public class Element {
 		slikaY = mBitmap.getHeight() / 2;
 		mSpeedX = sX;
 		mSpeedY = sY;
+		isAnimated = false;
 	}
 
-	public Element(Resources res) {
+	public Element(Resources res)
+	{
 		mBitmap = BitmapFactory.decodeResource(res, R.drawable.tank);
 		slikaX = mBitmap.getWidth();
 		slikaY = mBitmap.getHeight();
+		isAnimated = false;
 	}
 
-	public float getX() {
+	public float getX()
+	{
 		return mX;
 	}
 
-	public float getY() {
+	public float getY()
+	{
 		return mY;
 	}
 
-	public int getPoints() {
+	public int getPoints()
+	{
 		return points;
 	}
 
-	public void doDraw(Canvas canvas) {
+	public void doDraw(Canvas canvas)
+	{
 		canvas.drawBitmap(mBitmap, mX, mY, null);
-	}
 
-	/**
-	 * @param elapsedTime
-	 *            in ms.
-	 */
-	public void animate(long elapsedTime) {
+	}
+	public void animate(long elapsedTime)
+	{
+		if (this.isAnimated)
+		{
+			if(this.timeToChangeBitmap==0)
+			{
+				this.timeToChangeBitmap=System.currentTimeMillis();
+			
+				//TODO ko pride do vrha gremo nazaj 
+			}
+			else if(System.currentTimeMillis()-timeToChangeBitmap>200) // 200 ms
+			{
+				this.mBitmap=AnimatedBitmaps.get(positionBitmap);
+				this.timeToChangeBitmap=System.currentTimeMillis();;
+				this.positionBitmap++;
+				if(this.positionBitmap==AnimatedBitmaps.size())
+				{
+					this.positionBitmap=0;
+				}
+					
+			}
+		}
+
 		mX += mSpeedX * (elapsedTime / 20f);
 		mY += mSpeedY * (elapsedTime / 20f);
 	}
